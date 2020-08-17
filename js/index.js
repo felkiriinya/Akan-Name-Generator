38ca987ad6633e71b0780805326b554c5fb3052e
felkiriinya <felkiriinya@gmail.com>
2020-08-17 20:14:50 +0300

    getting input values from the form

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function inputInfo(){
+    //getting the value of the year
+    var year=document.getElementById("yearInput").value; 
+
+    //getting the value of the year
+    var month=document.getElementById("monthInput").value; 
+
+    //getting the value of the year
+    var day=document.getElementById("dayInput").value; 
+
+    //if male is checked true is returned and false if not
+    var male=document.getElementById("maleInput").checked;
+
+    //if female is checked true is returned and false if not
+    var male=document.getElementById("femaleInput").checked;
+
+    
+}
